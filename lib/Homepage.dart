52412c0e5fa10b4d051b26fb2d3471615bf73e66
Dipak <dipakmehta0910@gmail.com>
2020-09-16 11:56:49 +0530

    blog app done

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,6 +1,8 @@
 import 'package:blog_flutter/PhotoUpload.dart';
 import 'package:flutter/material.dart';
 import 'Authentication.dart';
+import 'Posts.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Homepage extends StatefulWidget {
 
@@ -20,6 +22,41 @@ class Homepage extends StatefulWidget {
 
 class _HomePageState extends State<Homepage> {
   
+  List<Posts> postsList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    DatabaseReference postRef = FirebaseDatabase.instance.reference().child("Posts");
+    postRef.once().then((DataSnapshot snap){
+        var KEYS = snap.value.keys;
+        var DATA = snap.value;
+
+        postsList.clear();
+
+        for(var individualKey in KEYS) {
+
+          Posts posts = new Posts(
+            
+            DATA[individualKey]['image'],
+            DATA[individualKey]['description'],
+            DATA[individualKey]['date'],
+            DATA[individualKey]['time'],
+            );
+
+            postsList.add(posts);
+
+        }
+
+        setState(() {
+          print('Length : $postsList.length');
+        });
+    });
+    
+  }
+
   void _logoutUser() async{
      try {
         await widget.auth.signOut();
@@ -35,9 +72,24 @@ class _HomePageState extends State<Homepage> {
       appBar: new AppBar(
         title: new Text("Home Page"),
       ),
-      body: new Container(
 
+      body: new Container(
+          child: postsList.length == 0 ? new Text("No Blog Post Available") : new ListView.builder(
+              itemCount: postsList.length,
+              itemBuilder: (_, index) {
+                
+                return PostsUI(postsList[index].image, 
+                  postsList[index].description,
+                  postsList[index].date,
+                  postsList[index].time
+
+                );
+
+              }
+
+          ),
       ),
+
       bottomNavigationBar: new BottomAppBar(
         color: Colors.pink,
         child: new Container(
@@ -76,5 +128,65 @@ class _HomePageState extends State<Homepage> {
 
     );
 
+  }
+
+  Widget PostsUI(String image, String description, String date, String time) {
+
+      return new Card(
+
+          elevation: 10.0,
+          margin: EdgeInsets.all(15.0),
+
+
+          child: new Container(
+              
+              padding: new EdgeInsets.all(14.0),
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: <Widget>[
+
+                    new Row(
+
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,  
+
+                      children: <Widget>[
+
+                          new Text(
+                            date,
+                            style: Theme.of(context).textTheme.subtitle,
+                            textAlign: TextAlign.center,
+                          ),
+
+                          new Text(
+                            time,
+                            style: Theme.of(context).textTheme.subtitle,
+                            textAlign: TextAlign.center,
+                          )
+
+
+                      ],
+
+                    ),
+
+                    SizedBox(height :10.0,),
+
+                    new Image.network(image, fit:BoxFit.cover),
+
+                    SizedBox(height :10.0,),
+                    
+                    new Text(
+                            description,
+                            style: Theme.of(context).textTheme.subhead,
+                            textAlign: TextAlign.center,
+                          ),
+
+                ],
+
+              ),
+            
+          ),
+
+      );
   }
 }
